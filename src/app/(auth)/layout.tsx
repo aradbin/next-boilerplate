1d23f5f2c8299543699859bcf840c442915751db
Arad Bin <aradbin@gmail.com>
2024-11-07 13:08:36 +0600

    layout update

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -1,14 +1,19 @@
-import { Header } from '@/components/public/header'
+import { Command } from 'lucide-react'
 import Image from 'next/image'
+import Link from 'next/link'
 
 export default function AuthLayout({ children }: { children: React.ReactNode }) {
   return (
     <>
-      <Header />
-      <div className="w-full lg:grid lg:min-h-[600px] lg:grid-cols-2 xl:min-h-[800px]">
+      {/* <Header /> */}
+      <Link href="/" className="fixed top-5 left-10 flex items-center justify-center space-x-2">
+        <Command />
+        <span className="font-bold">Acme Inc</span>
+      </Link>
+      <div className="w-full lg:grid lg:grid-cols-2">
         <div className="flex h-screen w-full items-center justify-center px-4">{children}</div>
         <div className="hidden bg-muted lg:block">
-          <Image src="/placeholder.svg" alt="Image" width="1920" height="1080" className="h-full w-full object-cover dark:brightness-[0.2] dark:grayscale" />
+          <Image src="/media/bg-dark.jpg" alt="Image" width="1920" height="1080" className="h-full w-full object-cover dark:brightness-[0.2] dark:grayscale" />
         </div>
       </div>
     </>
--- src/components/public/header.tsx
+++ src/components/public/header.tsx
@@ -1,13 +1,10 @@
-"use client"
+'use client'
 
 import * as React from 'react'
-import { CircleUser, Command, Github, Home, Menu, Package2 } from 'lucide-react'
+import { Command, Menu } from 'lucide-react'
 import Link from 'next/link'
 import { Sheet, SheetContent, SheetFooter, SheetHeader, SheetTrigger } from '../ui/sheet'
 import { Button } from '../ui/button'
-import { MagnifyingGlassIcon } from '@radix-ui/react-icons'
-import { Input } from '../ui/input'
-import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuLabel, DropdownMenuSeparator, DropdownMenuTrigger } from '../ui/dropdown-menu'
 import { ThemeToggle } from '../common/theme-toggle'
 import { usePathname } from 'next/navigation'
 import { cn } from '@/lib/utils'
@@ -17,16 +14,11 @@ export function Header() {
   return (
     <header className="sticky top-0 z-50 w-full border-b border-border/40 bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60 dark:border-border">
       <div className="flex h-14 items-center justify-between px-4 md:px-10">
-        
         {/* MobileNav */}
         <Sheet>
           <SheetTrigger asChild>
-            <Button
-              variant="outline"
-              size="icon"
-              className="shrink-0 md:hidden"
-            >
-              <Menu className='h-5 w-5' />
+            <Button variant="outline" size="icon" className="shrink-0 md:hidden">
+              <Menu className="h-5 w-5" />
               <span className="sr-only">Toggle Menu</span>
             </Button>
           </SheetTrigger>
@@ -34,9 +26,7 @@ export function Header() {
             <SheetHeader>
               <Link href="/" className="mr-4 flex items-center justify-center space-x-2">
                 <Command />
-                <span className="font-bold">
-                  Acme Inc
-                </span>
+                <span className="font-bold">Acme Inc</span>
               </Link>
             </SheetHeader>
             <div className="overflow-auto py-10">
@@ -59,9 +49,7 @@ export function Header() {
         <div className="flex md:hidden">
           <Link href="/" className="mr-4 flex items-center space-x-2">
             <Command />
-            <span className="font-bold">
-              Acme Inc
-            </span>
+            <span className="font-bold">Acme Inc</span>
           </Link>
         </div>
         {/* MobileNav */}
@@ -70,34 +58,20 @@ export function Header() {
         <div className="hidden md:flex gap-6 items-center text-sm">
           <Link href="/" className="flex items-center space-x-2">
             <Command />
-            <span className="hidden font-bold lg:inline-block">
-              Acme Inc
-            </span>
+            <span className="hidden font-bold lg:inline-block">Acme Inc</span>
           </Link>
-          <Link
-            href="/dashboard"
-            className={cn(
-              "transition-colors hover:text-foreground/80",
-              pathname === "/dashboard" ? "text-foreground" : "text-foreground/60"
-            )}
-          >
+          <Link href="/dashboard" className={cn('transition-colors hover:text-foreground/80', pathname === '/dashboard' ? 'text-foreground' : 'text-foreground/60')}>
             Dashboard
           </Link>
-          <Link
-            href="/pricing"
-            className={cn(
-              "transition-colors hover:text-foreground/80",
-              pathname === "/pricing" ? "text-foreground" : "text-foreground/60"
-            )}
-          >
+          <Link href="/pricing" className={cn('transition-colors hover:text-foreground/80', pathname === '/pricing' ? 'text-foreground' : 'text-foreground/60')}>
             Pricing
           </Link>
         </div>
         {/* MainNav */}
 
         <div className="flex items-center gap-2">
-          <Button className='hidden md:flex' asChild>
-            <Link href="/login">Login</Link>
+          <Button className="hidden md:flex" asChild>
+            <Link href="/login">Get Started</Link>
           </Button>
           <ThemeToggle />
         </div>
